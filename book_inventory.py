39a0502c44c448a2a388c7de632d150a0f0fa04d
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2022-06-23 19:46:35 +0530

    Add files via upload

---
+++ book_inventory.py
@@ -0,0 +1,17 @@
+from exception import ISBNAlreadyExistsError,ISBNDoesNotExistsError,BookNotAvailableError
+class BookInventory:
+    Book_List=[]
+    Isbn_List=[]
+    Issued_Book_List=[]
+    Issued_Isbn_List=[]
+    @staticmethod
+    def add_book(Book):
+        pass
+
+    @staticmethod
+    def issue_book(Issue):
+        pass
+
+    @staticmethod
+    def show_inventory():
+        pass
---
+++ issue.py
@@ -0,0 +1,5 @@
+class Issue:
+    def __init__(self,ISBN,student_name,issue_date):
+        self.ISBN=ISBN
+        self.student_name=student_name
+        self.issue_date=issue_date
