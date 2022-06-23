2b4e854bf56a2fc9fdbec732d71f370891062902
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2022-06-23 19:45:00 +0530

    Delete book.py

--- book.py
+++
@@ -1,7 +0,0 @@
-class Book:
-    def __init__(self,ISBN,book_name,publication,category):
-        self.ISBN=ISBN
-        self.book_name=book_name
-        self.publication=publication
-        self.category=category
-        self.issue=False
